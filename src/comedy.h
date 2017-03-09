cb594b183fefe28c00233221c171d760399c1915
Chantel Frizzell <Chantel Frizzell>
2017-03-09 12:00:43 -0800

    dvd info

--- src/comedy.h
+++ src/comedy.h
@@ -1,11 +1,70 @@
+// --------------------------- classic.h -----------------
+// 
+// 
+// Creation Date: 2/25/2017
+// Date of Last Modification: 
+// ------------------------------------------------------
+// Comedy movie: interface
+// Comedy DVD inherits from public DVD
+// ------------------------------------------------------
+
 #ifndef COMEDY_H
 #define COMEDY_H
 
 #include "dvd.h"
+#include <string>
+#include "dvd.h"
 
-class Comedy: public DVD
+/*
+* Subclass of DVD class.
+* 
+* Comedy movies are stored in AVL tree, sorted by Title and Release Year.
+*/
+class Comedy : public DVD
 {
+public:
+	/*
+	* Creates an instance of Comedy movie.
+	*/
+	Comedy(/*parameters to create comedy movie*/);
 
+	~Comedy();
+	
+	/*
+	* Returns title==, releaseYear==
+	*/
+	bool operator ==(const Comedy &rhs) const;
+
+	/*
+	* Returns title!=, releaseYear!=
+	*/
+	bool operator !=(const Comedy &rhs) const;
+
+	/*
+	* Returns < based on title and release year
+	*/
+	bool operator <(const Comedy &rhs) const;
+
+	/*
+	* Returns > based on title and release year
+	*/
+	bool operator >(const Comedy &rhs) const;
+
+	/*
+	* Returns title 
+	*/
+	std::string getTitle();
+
+	/*
+	* Returns release year
+	*/
+	int getReleaseYear();
+
+
+private:
+	Comedy();
+	int year;
+	std::string title;
 };
 
 #endif //COMEDY_H
--- src/drama.h
+++ src/drama.h
@@ -1,12 +1,69 @@
+// --------------------------- classic.h -----------------
+// 
+// Creation Date: 2/25/2017
+// Date of Last Modification: 
+// ------------------------------------------------------
+// Drama movie: interface
+// Drama DVD inherits from public DVD
+// ------------------------------------------------------
+
 #ifndef DRAMA_H
 #define DRAMA_H
 
 #include "dvd.h"
+#include <string>
+#include "dvd.h"
 
-class Drama: public DVD
+/*
+* Subclass of DVD class.
+*
+* Drama movies are stored in AVL tree, sorted by Director and Title.
+*/
+class Drama : public DVD
 {
+public:
+	/*
+	* Creates an instance of drama movie.
+	*/
+	Drama(/*parameters to create drama movie*/);
 
+	~Drama();
+
+	/*
+	* Returns title==, director==
+	*/
+	bool operator ==(const Drama &rhs) const;
+
+	/*
+	* Returns title!=, director!=
+	*/
+	bool operator !=(const Drama &rhs) const;
+
+	/*
+	* Returns < based on title and director
+	*/
+	bool operator <(const Drama &rhs) const;
+
+	/*
+	* Returns > based on title and director
+	*/
+	bool operator >(const Drama &rhs) const;
+
+	/*
+	* Returns title
+	*/
+	std::string getTitle();
+
+	/*
+	* Returns director
+	*/
+	int getDirector();
+
+
+private:
+	Drama();
+	std::string director;
+	std::string title;
 };
 
-
-#endif //DRAMA_H
+#endif //COMEDY_H
