c0f04c25eaa90aeaed6e5ac74968f9cc898ecb91
Jeremy DeHaan <dehaan.jeremiah@gmail.com>
2017-03-09 21:06:51 -0800

    minor changes to hashtable

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd src
-g++ -std=c++11 main.cpp store.cpp rentablestorage.cpp rentable.cpp -o ../test
+g++ -std=c++11 main.cpp store.cpp rentablestorage.cpp rentable.cpp hashtable.cpp -o ../test
 cd ..
--- src/hashtable.cpp
+++ src/hashtable.cpp
@@ -1,7 +1,13 @@
-#include "hashTable.h"
+#include "hashtable.h"
 
-	HashTable();
-	~HashTable();
+	HashTable()
+	{
+
+	}
+	~HashTable()
+	{
+		makeEmpty();
+	}
 
 	bool HashTable::insert(Customer* cust)
 	{
@@ -37,7 +43,7 @@
     /*
      * Retrieves a customer from table, points to it.
      */
-	bool retrieve(const Customer& cust, Customer*& retC)
+	bool HashTable::retrieve(const Customer& cust, Customer*& retC)
 	{
 		int i = 0;
 		bool found = false;
@@ -71,7 +77,7 @@
     /*
      * Removes a customer from the table
      */
-	bool removeCustomer(Customer& cust)
+	bool HashTable::removeCustomer(Customer& cust)
 	{
 		// we would do a lazy delete; however, not required for assignment. 
 	}
@@ -80,25 +86,28 @@
 	 * Resizes the table.
      * Requires additional helper functions: findPrime(), reinsertCustomer().
      */
-	void resize(int newSize)
+	void HashTable::resize(int newSize)
 	{
 		// not implemented in this assignment, but may be necessary in case of increased customers
 	}
 
 	//Returns h for a customer: customerId % MIN_TABLE_SIZE
-	int hash1(const Customer *cust)
+	int HashTable::hash1(const Customer *cust)
 	{
 		return cust->getCustomerID() % MIN_TABLE_SIZE;
 	}
 
 	//Function for double hashing.
-	int hash2(const Customer *cust)
+	int HashTable::hash2(const Customer *cust)
 	{
 		return (7 - (cust->getCustomerID() % 7));
 	}
 
 	//Empties out the hash table
-	void makeEmpty();
+	void HashTable::makeEmpty()
+	{
+
+	}
 };
 
 #endif //HASHTABLE_H
