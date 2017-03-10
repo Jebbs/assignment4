7e9e27c42ff160048d363c29f2861a394a89a613
Chantel Frizzell <chantelfrizzell@hotmail.com>
2017-03-10 11:27:38 -0800

    updates on customer

--- src/customer.cpp
+++ src/customer.cpp
@@ -47,46 +47,24 @@ void Customer::displayHistory() const
 }
 */
 
-bool Customer::operator==(const Customer& rhs) const  // do we need these? 
+bool Customer::operator==(const Customer& rhs) const 
 {
 	return customerID== rhs.getCustomerID();
 }
 
-bool Customer::operator!=(const Customer& rhs) const  // do we need these? 
+bool Customer::operator!=(const Customer& rhs) const 
 {
 	return customerID != rhs.getCustomerID();
 }
 
 bool Customer::borrowRentable(int borrowCount, Rentable& rentable)
 {
-	//RentalDetails transaction = new RentalDetails();
-	// or 
-
 	//RentalDetails has a reference to a rentable, so this needs to be done
 	//all at the same time.
 	RentalDetails transaction = {rentable, 0, borrowCount, false, "Borrow"};
-	
 
 	currentlyRenting.push_back(transaction);
-	//customerHistory;//push_back what? the same transaction?
-
-
-	//Each rentable has its rentalPeriodInDays set during construction
-	//I think below is not needed since it will always be set, so we don't have to set it manually
-
-	/*
-	// to set return date - NOT fully implemented, since out of the scope for this assignment.
-	switch (type)
-	{
-	case 0: 
-		//rental is a DVD
-		currentlyRenting[i].setRentalPeriodInDayse = DVD_RENTAL_PERIOD;
-		break;
-	case 1:
-		// this (and any other case) is for future implementation of other rentable types.
-		break;
-	}
-	*/
+	customerHistory.push_back(transaction);
 }
 
 bool Customer::returnRentable(int returnCount, Rentable& rentable)
@@ -101,31 +79,28 @@ bool Customer::returnRentable(int returnCount, Rentable& rentable)
 			//if so, need to remove transaction from currentlyRenting
 
 			RentalDetails transaction = currentlyRenting[i];
-			transaction.count -= returnCount;//minus equals and not  just equals?
+			transaction.count = returnCount;
 			transaction.returned = true;
 			transaction.action = "Return";
 
-			//addToCustomerHistory(transaction);
-
 			//update to reflect return 
 			currentlyRenting[i].count -= returnCount;
-			currentlyRenting[i].returned = true;
-			currentlyRenting[i].action = "Return";
 			
+			if (count == 0)
+			{
+				// no more on hand, so remove from vector
+				currentlyRenting.erase(i);
+			}
 			customerHistory.push_back(transaction);
-			//customerHistory;
 			break;
 		}
 		else
 		{
-			std::cerr << "INVALID RETURN REQUEST" << std::endl;
+			std::cerr << "ERROR: Invalid return request." << std::endl;
 		}
 	}
 
-	//emit error here
-
-	//"customer not renting this movie" or something like that.
-
+	cerr << "ERROR: Customer not currently renting " << rentable.getTitle() << endl;
 }
 
 void Customer::displayHistory() const
@@ -134,27 +109,25 @@ void Customer::displayHistory() const
 	std::cout << ", " << getCustomerFirstName();
 	std::cout << "  Customer ID: "<< customerID << std::endl;
 	
-	//RentalDetails transaction = new RentalDetails();
-	// or 
-	
 	for (int i = customerHistory.size() - 1; i >= 0; i--)
 	{
-
-
-
 		RentalDetails transaction = customerHistory[i];
 		std::cout << transaction.action << "     ";
 		std::cout << transaction.rental.getType() << "     ";
-		// cout << transaction.rental.subtypestuff
-		std::cout << transaction.rental.getTitle() << "     ";
-		//if (transaction.rental.getType() == classic)
+		
+		// optional: print out subtype here
+		// if subtype printed, need to differentiate between 
+		// classic and others. 
+		// if (transaction.rental.getType() == classic)
 		// {
 		//     do the date thats necessary here 
 		//     do the actors necessary here
 		// else 
-		// {
+		// { othere types }
+
+		std::cout << transaction.rental.getTitle() << "     ";
 		std::cout << transaction.rental.getReleaseYear();
-		// }
+
 		std::cout << std::endl;
 	}
 }
