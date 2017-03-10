4a9789b0b54fab328e2b8e4b9850f9868e83d5b0
Chantel Frizzell <chantelfrizzell@hotmail.com>
2017-03-10 11:46:21 -0800

    comments added to command.h

--- src/command.h
+++ src/command.h
@@ -1,29 +1,39 @@
+/*******************************************************************************
+ * Command
+ * 
+ * Author: 
+ * Jeremy DeHaan, Chantel Frizzell
+ *
+ * Date Last Modified: 
+ * 2017-03-10
+ *
+ * The Command class defines a template for all
+ * types of commands.
+ *
+ * The command class then process the commands.
+ ******************************************************************************/
+
 #ifndef COMMAND_H
 #define COMMAND_H
 
 //forward decl
 class Store;
 
-
-/*
- * The Command class defines a template for all
- * types of commands.
- */
 class Command
 {
     //so that a store can give a command an instance of itself when creating
     friend class Store;
+
 public:
 
     /*
-     * Process the command for this Command.
+     * The command class process the command for this Command
      */
     virtual bool processCommand() = 0;
 
 private:
     //the instance of the store this command will operate on
     Store* storeInst;
-
 };
 
 #endif //COMMAND_H
