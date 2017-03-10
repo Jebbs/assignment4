21593b8e4563ad98a78fd704c0cb1929da4d341d
Jeremy DeHaan <dehaan.jeremiah@gmail.com>
2017-03-09 19:09:40 -0800

    Started fixes rentable.cpp

--- src/rentable.cpp
+++ src/rentable.cpp
@@ -1,6 +1,47 @@
 #include "rentable.h"
 
 
+Rentable::Rentable(RentableType type, int stock, std::string Title, int releaseYear, int rentalPeriodInDays)
+{
+	this->type = type;
+	this->stock = stock;
+	this->title = Title;
+	this->releaseYear = releaseYear;
+	this->rentalPeriodInDays = rentalPeriodInDays;
+}
+
+bool Rentable::inStock() const
+{
+	return stock>0;
+}
+
+bool Rentable::removeFromStock(int amount)
+{
+	if(stock-amount >= 0)
+	{
+		stock-=amount;
+		return true;
+	}
+
+	return false;
+}
+
+void Rentable::addToStock(int amount);
+
+RentableType Rentable::getType() const;
+
+int Rentable::getSubtype() const;
+
+std::string Rentable::getTitle() const;
+
+int Rentable::getReleaseYear() const;
+
+int Rentable::getRentalPeriodInDays() const;
+
+void Rentable::setRentalPeriodInDays(int rentalPeriod);
+
+int Rentable::getStockCount() const;
+
 bool Rentable::operator==(const Rentable& rentable) const
 {
     return this->equalTo(rentable);
