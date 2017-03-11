92752b7455bf4c8ec8b88a1ffbc2e4910ee2d7ab
Chantel <chantelfrizzell@hotmail.com>
2017-03-11 10:25:42 -0800

    comments updated on header

--- src/dvd.h
+++ src/dvd.h
@@ -9,14 +9,15 @@
  *
  * The DVD class models a rentable type DVD.
  * DVD is the only rentable in the scope of this assignment.
-
-
-To store the data in the system, the items should be sorted as follows:
-comedy movies (‘F’) sorted by Title, then Year it released 
-dramas (‘D’) are sorted by Director, then Title 
-classics (‘C’) are sorted by Release date, then Major actor
-You can assume that each item is uniquely identified by its complete set of sorting attributes.  
-
+ * 
+ * To store the data in the system, the items should be sorted as follows:
+ * 
+ * comedy (‘F’) -> by Title, then Year it released 
+ * dramas (‘D’) -> by Director, then Title 
+ * classic (‘C’) -> by Release date, then Major Actor
+ * 
+ * Assumption: Each item is uniquely identified by its complete set 
+ * of sorting attributes.  
  ******************************************************************************/
 
 #ifndef MOVIE_H
